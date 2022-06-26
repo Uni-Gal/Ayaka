5b9c9d077716ed49ac75e3b615b1d502382cd17a
Yuyi Wang <Strawberry_Str@hotmail.com>
2022-06-27 01:51:38 +0800

    Remove useless exports.

--- gal-locale/src/icu/windows.rs
+++ gal-locale/src/icu/windows.rs
@@ -1,5 +1,4 @@
 pub use windows_sys::Win32::Globalization::{
-    uenum_close, uenum_openCharStringsEnumeration, uloc_acceptLanguage, uloc_forLanguageTag,
-    uloc_getDefault, UAcceptResult, UErrorCode, ULOC_ACCEPT_FAILED, U_BUFFER_OVERFLOW_ERROR,
-    U_ZERO_ERROR,
+    uenum_close, uenum_openCharStringsEnumeration, uloc_acceptLanguage, uloc_getDefault,
+    UAcceptResult, UErrorCode, ULOC_ACCEPT_FAILED, U_BUFFER_OVERFLOW_ERROR, U_ZERO_ERROR,
 };
