74529532529bd4f0bd34e6dd393d106a02b2775d
Berrysoft <Strawberry_Str@hotmail.com>
2022-06-27 18:34:05 +0800

    Fix icu env exports.

--- gal-locale/src/icu/env.rs
+++ gal-locale/src/icu/env.rs
@@ -1,46 +1,7 @@
-#![allow(non_snake_case)]
-
-use rust_icu_sys::*;
-
 pub use rust_icu_sys::{
-    UAcceptResult,
+    uenum_close, uenum_openCharStringsEnumeration, uloc_acceptLanguage, uloc_canonicalize,
+    uloc_getDefault, UAcceptResult,
     UAcceptResult::ULOC_ACCEPT_FAILED,
     UErrorCode,
     UErrorCode::{U_BUFFER_OVERFLOW_ERROR, U_ZERO_ERROR},
 };
-
-pub unsafe fn uloc_getDefault() -> *const ::std::os::raw::c_char {
-    versioned_function!(rust_icu_sys::uloc_getDefault)()
-}
-
-pub unsafe fn uloc_acceptLanguage(
-    result: *mut ::std::os::raw::c_char,
-    resultAvailable: i32,
-    outResult: *mut UAcceptResult,
-    acceptList: *mut *const ::std::os::raw::c_char,
-    acceptListCount: i32,
-    availableLocales: *mut UEnumeration,
-    status: *mut UErrorCode,
-) -> i32 {
-    versioned_function!(rust_icu_sys::uloc_acceptLanguage)(
-        result,
-        resultAvailable,
-        outResult,
-        acceptList,
-        acceptListCount,
-        availableLocales,
-        status,
-    )
-}
-
-pub unsafe fn uenum_openCharStringsEnumeration(
-    strings: *const *const ::std::os::raw::c_char,
-    count: i32,
-    ec: *mut UErrorCode,
-) -> *mut UEnumeration {
-    versioned_function!(rust_icu_sys::uenum_openCharStringsEnumeration)(strings, count, ec)
-}
-
-pub unsafe fn uenum_close(en: *mut UEnumeration) {
-    versioned_function!(rust_icu_sys::uenum_close)(en)
-}
