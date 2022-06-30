dc6ee79241db9c0569084002d435b94e346387d3
Berrysoft <Strawberry_Str@hotmail.com>
2022-07-01 00:04:51 +0800

    Fix env.rs

--- gal-locale/src/icu/windows.rs
+++ gal-locale/src/icu/windows.rs
@@ -1,5 +1,7 @@
 pub use windows_sys::Win32::Globalization::{
-    uenum_close, uenum_openCharStringsEnumeration, uloc_acceptLanguage, uloc_canonicalize,
-    uloc_getDefault, UAcceptResult, UErrorCode, ULOC_ACCEPT_FAILED, U_BUFFER_OVERFLOW_ERROR,
-    U_ZERO_ERROR,
+    uenum_close as imp_uenum_close,
+    uenum_openCharStringsEnumeration as imp_uenum_openCharStringsEnumeration,
+    uloc_acceptLanguage as imp_uloc_acceptLanguage, uloc_canonicalize as imp_uloc_canonicalize,
+    uloc_getDefault as imp_uloc_getDefault, UAcceptResult, UErrorCode, ULOC_ACCEPT_FAILED,
+    U_BUFFER_OVERFLOW_ERROR, U_ZERO_ERROR,
 };
