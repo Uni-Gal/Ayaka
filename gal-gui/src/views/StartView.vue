f09022ef7878da729c244e748199c62fe2c2e43a
Berrysoft <Strawberry_Str@hotmail.com>
2022-07-06 15:29:24 +0800

    Add progress bar

--- gal-gui/src/views/StartView.vue
+++ gal-gui/src/views/StartView.vue
@@ -10,13 +10,14 @@ export default {
     data() {
         return {
             text: "",
+            progress: 0,
         }
     },
     async created() {
         listen('gal://open_status', e => {
             console.log(e.payload)
             const status = e.payload as OpenGameStatus
-            this.text = this.status_to_text(status)
+            [this.text, this.progress] = this.status_to_text(status)
             if (OpenGameStatusType[status.t] == OpenGameStatusType.Loaded) {
                 router.replace("/home")
             }
@@ -24,18 +25,18 @@ export default {
         await open_game()
     },
     methods: {
-        status_to_text(s: OpenGameStatus): string {
+        status_to_text(s: OpenGameStatus): [string, number] {
             switch (OpenGameStatusType[s.t]) {
                 case OpenGameStatusType.LoadProfile:
-                    return `Loading profile "${s.text}"...`
+                    return [`Loading profile "${s.text}"...`, 25]
                 case OpenGameStatusType.CreateRuntime:
-                    return "Creating runtime..."
+                    return ["Creating runtime...", 50]
                 case OpenGameStatusType.LoadPlugin:
-                    return `Loading plugin "${s.text}"...`
+                    return [`Loading plugin "${s.text}"...`, 75]
                 case OpenGameStatusType.Loaded:
-                    return "Loaded."
+                    return ["Loaded.", 100]
                 default:
-                    return ""
+                    return ["", 0]
             }
         }
     }
@@ -43,9 +44,16 @@ export default {
 </script>
 
 <template>
-    <div class="content">
-        <div class="d-grid gap-4 col-4 mx-auto">
-            <p>{{ text }}</p>
-        </div>
+    <div class="progress progress-bottom">
+        <div class="progress-bar" role="progressbar" v-bind:style='`width: ${progress}%`'>{{ text }}</div>
     </div>
 </template>
+
+<style>
+.progress-bottom {
+    position: absolute;
+    top: 100%;
+    width: 100%;
+    transform: translateY(-100%);
+}
+</style>
