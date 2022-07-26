e9ec22d46ac5dc606bcacc57974264394792b49a
Berrysoft <Strawberry_Str@hotmail.com>
2022-07-26 18:22:22 +0800

    Add doc for gal-fallback.

--- utils/gal-fallback/src/lib.rs
+++ utils/gal-fallback/src/lib.rs
@@ -4,6 +4,7 @@
 //! if a value or a part of value doesn't exist.
 
 /// Stores two [`Option`], and provides functionality to fallback.
+///
 /// Basically, you provides a function returns [`Option`],
 /// and [`Fallback`] handles the fallback.
 /// ```
@@ -14,45 +15,63 @@
 /// let num = fallback.and_then(|s| s.parse::<i32>().ok());
 /// assert_eq!(num, Some(123));
 /// ```
+/// And you can map the [`Fallback`]:
+/// ```
+/// # use gal_fallback::Fallback;
+/// let data = Some(123);
+/// let base_data = Some(123456);
+/// let fallback = Fallback::new(data, base_data);
+/// let fallback = fallback.map(|i| i.to_string());
+/// let s = fallback.and_then(|s| if s.len() > 3 { Some(s) } else { None });
+/// assert_eq!(s, Some("123456".to_string()));
+/// ```
 pub struct Fallback<T> {
     data: Option<T>,
     base_data: Option<T>,
 }
 
 impl<T> Fallback<T> {
+    /// Creates a new [`Fallback`].
     pub const fn new(data: Option<T>, base_data: Option<T>) -> Self {
         Self { data, base_data }
     }
 
+    /// Returns `false` if both `data` and `base_data` are [`None`].
     pub const fn is_some(&self) -> bool {
         self.data.is_some() || self.base_data.is_some()
     }
 
+    /// Converts from `&Fallback<T>` to `Fallback<&T>`.
     pub const fn as_ref(&self) -> Fallback<&T> {
         Fallback::new(self.data.as_ref(), self.base_data.as_ref())
     }
 
+    /// Fallbacks the data or part of data.
     pub fn and_then<V>(self, mut f: impl FnMut(T) -> Option<V>) -> Option<V> {
         self.data
             .and_then(|t| f(t))
             .or_else(|| self.base_data.and_then(|t| f(t)))
     }
 
+    /// Maps to a new [`Fallback`].
     pub fn map<V>(self, mut f: impl FnMut(T) -> V) -> Fallback<V> {
         Fallback::new(self.data.map(|t| f(t)), self.base_data.map(|t| f(t)))
     }
 
+    /// Exacts the `data` and `base_data`.
     pub fn unzip(self) -> (Option<T>, Option<T>) {
         (self.data, self.base_data)
     }
 }
 
 impl<T> Fallback<Option<T>> {
+    /// Converts from `Fallback<Option<T>>` to `Fallback<T>`.
     pub fn flatten(self) -> Fallback<T> {
         Fallback::new(self.data.flatten(), self.base_data.flatten())
     }
 }
 
+#[doc(hidden)]
 pub trait IsEmpty2 {
     fn is_empty2(&self) -> bool;
 }
@@ -70,6 +89,7 @@ impl<T> IsEmpty2 for Vec<T> {
 }
 
 impl<T: IsEmpty2> Fallback<T> {
+    /// Treats the empty container as [`None`] and fallbacks.
     pub fn and_any(self) -> Option<T> {
         self.and_then(|s| if s.is_empty2() { None } else { Some(s) })
     }
@@ -85,6 +105,7 @@ impl<T> From<Fallback<T>> for Option<T> {
     }
 }
 
+#[doc(hidden)]
 pub struct FallbackIter<A> {
     data: A,
     base_data: A,
@@ -117,11 +138,58 @@ impl<T> IntoIterator for Fallback<Vec<T>> {
     }
 }
 
+/// This trait helps to create a new fallback type.
+///
+/// The code
+/// ```
+/// # use gal_fallback::FallbackSpec;
+/// #[derive(FallbackSpec)]
+/// struct Foo {
+///     data1: i32,
+///     data2: String,
+/// }
+/// ```
+/// is the same as
+/// ```
+/// # use gal_fallback::*;
+/// struct Foo {
+///     data1: i32,
+///     data2: String,
+/// }
+///
+/// struct FallbackFoo {
+///     data1: Fallback<i32>,
+///     data2: Fallback<String>,
+/// }
+///
+/// impl FallbackSpec for Foo {
+///     type SpecType = FallbackFoo;
+/// }
+///
+/// impl From<Fallback<Foo>> for FallbackFoo {
+///     fn from(data: Fallback<Foo>) -> Self {
+///         let (data, base_data) = data.unzip();
+///         let (data1, data2) = match data {
+///             Some(data) => (Some(data.data1), Some(data.data2)),
+///             None => (None, None),
+///         };
+///         let (base_data1, base_data2) = match base_data {
+///             Some(data) => (Some(data.data1), Some(data.data2)),
+///             None => (None, None),
+///         };
+///         Self {
+///             data1: Fallback::new(data1, base_data1),
+///             data2: Fallback::new(data2, base_data2),
+///         }
+///     }
+/// }
+/// ```
 pub trait FallbackSpec: Sized {
     type SpecType: From<Fallback<Self>>;
 }
 
 impl<T: FallbackSpec> Fallback<T> {
+    /// Get the specialized fallback type.
     pub fn spec(self) -> T::SpecType {
         T::SpecType::from(self)
     }
