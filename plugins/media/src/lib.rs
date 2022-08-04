8b3b463eebc253fb9299c9069e2b3c12bc81e7b2
Yuyi Wang <Strawberry_Str@hotmail.com>
2022-08-04 16:39:58 +0800

    Remove \voice

--- plugins/media/src/lib.rs
+++ plugins/media/src/lib.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 use gal_bindings::*;
 
 #[export]
@@ -7,35 +9,28 @@ fn plugin_type() -> PluginType {
 
 #[export]
 fn text_commands() -> &'static [&'static str] {
-    &["bg", "bgm", "efm", "voice", "video"]
+    &["bg", "bgm", "efm", "video"]
 }
 
 fn file(
     args: Vec<String>,
-    ctx: TextProcessContext,
-    game_prop: &str,
+    base_dir: impl AsRef<Path>,
     prop: &str,
     exs: &[&str],
 ) -> TextProcessResult {
     assert_eq!(args.len(), 1);
+    let base_dir = base_dir.as_ref();
     log::info!(
-        "File {:?}, {:?}, {}, {}, {:?}",
+        "File {:?}, {:?}, {}, {:?}",
         args,
-        ctx,
-        game_prop,
+        base_dir.display(),
         prop,
         exs
-    );
-    let res_dir = ctx.root_path.join(
-        ctx.game_props
-            .get(game_prop)
-            .map(|s| s.as_str())
-            .unwrap_or(""),
     );
     let mut res = TextProcessResult::default();
     if let Some(path) = exs
         .into_iter()
-        .map(|ex| res_dir.join(&args[0]).with_extension(ex))
+        .map(|ex| base_dir.join(&args[0]).with_extension(ex))
         .filter(|p| p.exists())
         .next()
     {
@@ -45,32 +40,44 @@ fn file(
     res
 }
 
+fn file_ctx(
+    args: Vec<String>,
+    ctx: TextProcessContext,
+    game_prop: &str,
+    prop: &str,
+    exs: &[&str],
+) -> TextProcessResult {
+    file(
+        args,
+        ctx.root_path.join(
+            ctx.game_props
+                .get(game_prop)
+                .map(|s| s.as_str())
+                .unwrap_or(""),
+        ),
+        prop,
+        exs,
+    )
+}
+
 #[export]
 fn bg(args: Vec<String>, ctx: TextProcessContext) -> TextProcessResult {
-    file(args, ctx, "bgs", "bg", &["png", "jpg", "gif"])
+    file_ctx(args, ctx, "bgs", "bg", &["png", "jpg", "gif"])
 }
 
 #[export]
 fn bgm(args: Vec<String>, ctx: TextProcessContext) -> TextProcessResult {
-    file(args, ctx, "bgms", "bgm", &["mp3"])
+    file_ctx(args, ctx, "bgms", "bgm", &["mp3"])
 }
 
 #[export]
 fn efm(args: Vec<String>, ctx: TextProcessContext) -> TextProcessResult {
-    file(args, ctx, "efms", "efm", &["mp3"])
-}
-
-#[export]
-fn voice(args: Vec<String>, _ctx: TextProcessContext) -> TextProcessResult {
-    assert_eq!(args.len(), 1);
-    let mut res = TextProcessResult::default();
-    res.props.insert("voice_id".to_string(), args[0].clone());
-    res
+    file_ctx(args, ctx, "efms", "efm", &["mp3"])
 }
 
 #[export]
 fn video(args: Vec<String>, ctx: TextProcessContext) -> TextProcessResult {
-    file(args, ctx, "videos", "video", &["mp4"])
+    file_ctx(args, ctx, "videos", "video", &["mp4"])
 }
 
 #[export]
@@ -84,30 +91,17 @@ fn process_action(mut ctx: ActionProcessContext) -> Action {
             }
         }
     }
-    let voice_id = ctx
-        .action
-        .props
-        .get("voice_id")
-        .map(|s| s.parse::<usize>().unwrap())
-        .or_else(|| {
-            ctx.last_action
-                .as_ref()
-                .and_then(|act| act.props.get("voice_id"))
-                .map(|s| s.parse::<usize>().unwrap() + 1)
-        })
-        .unwrap_or_default()
-        .to_string();
-    ctx.action
-        .props
-        .insert("voice_id".to_string(), voice_id.clone());
+    let voice_id = ctx.action.cur_act.to_string();
     let res = file(
         vec![voice_id],
-        TextProcessContext {
-            root_path: ctx.root_path,
-            game_props: ctx.game_props,
-            frontend: ctx.frontend,
-        },
-        "voices",
+        ctx.root_path
+            .join(
+                ctx.game_props
+                    .get("voices")
+                    .map(|s| s.as_str())
+                    .unwrap_or(""),
+            )
+            .join(&ctx.action.cur_para),
         "voice",
         &["mp3"],
     );
