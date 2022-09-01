ef1172c34fbc0308484c3deeaa8e073b07053b6a
Yuyi Wang <Strawberry_Str@hotmail.com>
2022-09-01 22:17:50 +0800

    Add ident for gal-latex.

--- bins/gal-latex/src/writer.rs
+++ bins/gal-latex/src/writer.rs
@@ -3,14 +3,18 @@ use tokio::{fs::File, io::AsyncWriteExt};
 
 pub struct LaTeXWriter {
     file: File,
+    ident: usize,
 }
 
 impl LaTeXWriter {
     pub fn new(file: File) -> Self {
-        Self { file }
+        Self { file, ident: 0 }
     }
 
     pub async fn write(&mut self, text: impl AsRef<str>) -> Result<&mut Self> {
+        self.file
+            .write_all(" ".repeat(self.ident * 4).as_bytes())
+            .await?;
         self.file.write_all(text.as_ref().as_bytes()).await?;
         Ok(self)
     }
@@ -20,20 +24,15 @@ impl LaTeXWriter {
         cmd: impl AsRef<str>,
         args: impl IntoIterator<Item = impl AsRef<str>>,
     ) -> Result<&mut Self> {
-        self.file
-            .write_all(
-                format!("\\{}{}\n", cmd.as_ref(), unsafe {
-                    String::from_utf8_unchecked(
-                        args.into_iter()
-                            .map(|arg| format!("{{{}}}", arg.as_ref()))
-                            .flat_map(|s| s.into_bytes())
-                            .collect::<Vec<_>>(),
-                    )
-                })
-                .as_bytes(),
+        self.write(format!("\\{}{}\n", cmd.as_ref(), unsafe {
+            String::from_utf8_unchecked(
+                args.into_iter()
+                    .map(|arg| format!("{{{}}}", arg.as_ref()))
+                    .flat_map(|s| s.into_bytes())
+                    .collect::<Vec<_>>(),
             )
-            .await?;
-        Ok(self)
+        }))
+        .await
     }
 
     pub async fn command0(&mut self, cmd: impl AsRef<str>) -> Result<&mut Self> {
@@ -46,6 +45,9 @@ impl LaTeXWriter {
         f: impl FnOnce(&'a mut Self) -> F,
     ) -> Result<&'a mut Self> {
         self.command("begin", [env.as_ref()]).await?;
-        f(self).await?.command("end", [env]).await
+        self.ident += 1;
+        let this = f(self).await?;
+        this.ident -= 1;
+        this.command("end", [env]).await
     }
 }
