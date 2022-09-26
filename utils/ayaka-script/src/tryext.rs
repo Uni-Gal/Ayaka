1bd6bcb5fe18d14aa69797a2114e698d37ce773b
Yuyi Wang <Strawberry_Str@hotmail.com>
2022-09-26 15:07:46 +0800

    Simplify TryExt.

--- utils/ayaka-script/src/tryext.rs
+++ utils/ayaka-script/src/tryext.rs
@@ -6,46 +6,31 @@ use std::{
 
 #[doc(hidden)]
 pub trait TryExt: Try {
-    fn ok_or_inspect(self, f: impl FnOnce(&Self::Residual)) -> Option<Self::Output>;
+    fn ok_or_log(self, msg: &str) -> Option<Self::Output>;
 
-    fn ok_or_log(self, msg: &str) -> Option<Self::Output>
-    where
-        Self: Sized,
-        Self::Residual: Debug,
-    {
-        self.ok_or_inspect(|e| warn!("{}: {:?}", msg, e))
-    }
-
-    fn unwrap_or_default_inspect(self, f: impl FnOnce(&Self::Residual)) -> Self::Output;
-
-    fn unwrap_or_default_log(self, msg: &str) -> Self::Output
-    where
-        Self: Sized,
-        Self::Residual: Debug,
-    {
-        self.unwrap_or_default_inspect(|e| warn!("{}: {:?}", msg, e))
-    }
+    fn unwrap_or_default_log(self, msg: &str) -> Self::Output;
 }
 
 impl<T: Try> TryExt for T
 where
     T::Output: Default,
+    T::Residual: Debug,
 {
-    fn ok_or_inspect(self, f: impl FnOnce(&Self::Residual)) -> Option<Self::Output> {
+    fn ok_or_log(self, msg: &str) -> Option<Self::Output> {
         match self.branch() {
             ControlFlow::Continue(v) => Some(v),
             ControlFlow::Break(e) => {
-                f(&e);
+                warn!("{msg}: {e:?}");
                 None
             }
         }
     }
 
-    fn unwrap_or_default_inspect(self, f: impl FnOnce(&Self::Residual)) -> Self::Output {
+    fn unwrap_or_default_log(self, msg: &str) -> Self::Output {
         match self.branch() {
             ControlFlow::Continue(v) => v,
             ControlFlow::Break(e) => {
-                f(&e);
+                warn!("{msg}: {e:?}");
                 Default::default()
             }
         }
