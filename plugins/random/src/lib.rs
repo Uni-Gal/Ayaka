d4a57214d72e1e8de860d436c63bcd24f63f9e7a
Yuyi Wang <Strawberry_Str@hotmail.com>
2022-12-16 15:10:01 +0800

    Move random to unknown.

--- plugins/random/src/lib.rs
+++ plugins/random/src/lib.rs
@@ -1,29 +1,23 @@
 #![deny(unsafe_code)]
-#![feature(once_cell)]
 
 use ayaka_bindings::*;
-use rand::{rngs::StdRng, Rng, SeedableRng};
-use std::sync::{LazyLock, Mutex};
-use trylog::macros::*;
 
 #[export]
 fn plugin_type() -> PluginType {
     PluginType::default()
 }
 
-static RNG: LazyLock<Mutex<StdRng>> = LazyLock::new(|| Mutex::new(StdRng::from_entropy()));
+#[import("rand")]
+extern "C" {
+    fn __rnd(start: i64, end: i64) -> i64;
+}
 
 #[export]
 fn rnd(args: Vec<RawValue>) -> RawValue {
-    unwrap_or_default_log!(
-        RNG.lock().map(|mut rng| {
-            let res = match args.len() {
-                0 => rng.gen(),
-                1 => rng.gen_range(0..args[0].get_num()),
-                _ => rng.gen_range(args[0].get_num()..args[1].get_num()),
-            };
-            RawValue::Num(res)
-        }),
-        "Cannot get random engine"
-    )
+    let res = match args.len() {
+        0 => __rnd(0, i64::MAX),
+        1 => __rnd(0, args[0].get_num()),
+        _ => __rnd(args[0].get_num(), args[1].get_num()),
+    };
+    RawValue::Num(res)
 }
