85171701201a21ca69941ae92575f5c4c1163118
Yuyi Wang <Strawberry_Str@hotmail.com>
2023-02-15 01:38:39 +0800

    Use result as response type.

--- bins/ayaka-gui/src-tauri/src/asset_resolver.rs
+++ bins/ayaka-gui/src-tauri/src/asset_resolver.rs
@@ -2,7 +2,7 @@ use axum::{
     body::Body,
     extract::Path,
     http::{header::CONTENT_TYPE, Request, StatusCode},
-    response::{IntoResponse, Response},
+    response::IntoResponse,
     routing::get,
     Router, Server,
 };
@@ -31,10 +31,11 @@ fn vfs_error_response(err: VfsError) -> (StatusCode, String) {
     (code, msg)
 }
 
-async fn fs_resolver(Path(path): Path<String>) -> Response {
+async fn fs_resolver(Path(path): Path<String>) -> impl IntoResponse {
     let path = ROOT_PATH.get().unwrap().join(path).unwrap();
     match path.open_file() {
         Ok(mut file) => {
+            // TODO: `read_to_end` may fail and doesn't return 200.
             log::debug!("Get FS {} 200", path.as_str());
             let mime = mime_guess::from_path(path.as_str()).first_or_octet_stream();
             // We choose to read_to_end, because in most release cases, the files should be in a TAR.
@@ -46,23 +47,25 @@ async fn fs_resolver(Path(path): Path<String>) -> Response {
                 .unwrap_or(65536);
             let mut buffer = Vec::with_capacity(length);
             match file.read_to_end(&mut buffer) {
-                Ok(_) => ([(CONTENT_TYPE, mime.to_string())], buffer).into_response(),
-                Err(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response(),
+                Ok(_) => Ok(([(CONTENT_TYPE, mime.to_string())], buffer)),
+                // The error types are temporarily the same (StatusCode, String)
+                // so we don't declare a new one.
+                Err(e) => Err((StatusCode::INTERNAL_SERVER_ERROR, e.to_string())),
             }
         }
         Err(err) => {
             let (code, msg) = vfs_error_response(err);
             log::debug!("Get FS {} {}", path.as_str(), code.as_u16());
-            (code, msg).into_response()
+            Err((code, msg))
         }
     }
 }
 
-async fn resolver<R: Runtime>(app: AppHandle<R>, req: Request<Body>) -> Response {
+async fn resolver<R: Runtime>(app: AppHandle<R>, req: Request<Body>) -> impl IntoResponse {
     if let Some(asset) = app.asset_resolver().get(req.uri().path().to_string()) {
-        ([(CONTENT_TYPE, asset.mime_type)], asset.bytes).into_response()
+        Ok(([(CONTENT_TYPE, asset.mime_type)], asset.bytes))
     } else {
-        (StatusCode::NOT_FOUND, ()).into_response()
+        Err(StatusCode::NOT_FOUND)
     }
 }
 
