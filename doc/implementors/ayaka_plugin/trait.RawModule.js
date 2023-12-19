d61798a53b28b4be178a0e8a6cfadbc4d3148616
Berrysoft <Berrysoft@users.noreply.github.com>
2023-12-19 14:00:27 +0000

    deploy: ef31c8a74a7d28ad0da868c6a8f319263184317c

--- doc/implementors/ayaka_plugin/trait.RawModule.js
+++
@@ -1,5 +0,0 @@
-(function() {var implementors = {
-"ayaka_plugin_wasmer":[["impl RawModule for <a class=\"struct\" href=\"ayaka_plugin_wasmer/struct.WasmerModule.html\" title=\"struct ayaka_plugin_wasmer::WasmerModule\">WasmerModule</a>"]],
-"ayaka_plugin_wasmi":[["impl RawModule for <a class=\"struct\" href=\"ayaka_plugin_wasmi/struct.WasmiModule.html\" title=\"struct ayaka_plugin_wasmi::WasmiModule\">WasmiModule</a>"]],
-"ayaka_plugin_wasmtime":[["impl <a class=\"trait\" href=\"ayaka_plugin/trait.RawModule.html\" title=\"trait ayaka_plugin::RawModule\">RawModule</a> for <a class=\"struct\" href=\"ayaka_plugin_wasmtime/struct.WasmtimeModule.html\" title=\"struct ayaka_plugin_wasmtime::WasmtimeModule\">WasmtimeModule</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
